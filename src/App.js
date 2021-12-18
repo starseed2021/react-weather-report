6ebe6a1382d2b69b8a7384fa8e9472fd248f6155
starseed2021 <tiff.harper18@gmail.com>
2021-12-18 17:51:25 -0500

    Followed another weather app template; added conditionals to change colors to various temperatures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+// import Temperature from './components/Temperature'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [temperatureValue, setTemperatureValue] = useState(30);
+
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+
+  const increaseTemperature = () => {
+    const newTemperature = temperatureValue + 1;
+    setTemperatureValue(newTemperature);
+
+    // UPDATING THE COLORS TO DIFFERENT TEMPERATURES
+    if (newTemperature >= 45) {
+      setTemperatureColor('chilly');
+    } else if (newTemperature >= 60) {
+      setTemperatureColor('temperate');
+    } else if (newTemperature >= 70) {
+      setTemperatureColor('warm');
+    } else if (newTemperature >= 85 && newTemperature <= 98) {
+      setTemperatureColor('hot');
+    }
+  };
+  
+  const decreaseTemperature = () => {
+    const newTemperature = temperatureValue - 1;
+    setTemperatureValue(temperatureValue - 1);
+
+    if (newTemperature < 30 && newTemperature >= 20) {
+      setTemperatureColor('cold');
+    }
+  };
+
+
+	return (
+		<div className='app-container'>
+			<div className='temperature-display-container'>
+      <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°F</div>
+			</div>
+			<div className='button-container'>
+        <button onClick={() => setTemperatureValue(increaseTemperature)}>+</button>
+				<button onClick={() => setTemperatureValue(decreaseTemperature)}>-</button>
+			</div>
+		</div>
+	);
+};
+
 
 export default App;
