cf80569868ba7a9ad5a783fed8fffe8db6723559
starseed2021 <tiff.harper18@gmail.com>
2021-12-19 21:31:48 -0500

    Created & implemented Temperature component js. and .css; changed color style of app; added a header

--- src/App.js
+++ src/App.js
@@ -1,60 +1,19 @@
 import React from 'react';
-import { useState } from 'react';
-// import Temperature from './components/Temperature'
+import Temperature from './components/Temperature'
+import './App.css';
 
 const App = () => {
-  const [temperatureValue, setTemperatureValue] = useState(30);
-
-  const [temperatureColor, setTemperatureColor] = useState('cold');
-
-  const increaseTemperature = () => {
-    const newTemperature = temperatureValue + 1;
-    setTemperatureValue(newTemperature);
-
-    // UPDATING THE COLORS TO DIFFERENT TEMPERATURES
-    if (newTemperature > 40 && newTemperature <= 50) {
-      setTemperatureColor('chilly');
-    } else if (newTemperature > 50 && newTemperature <= 60) {
-      setTemperatureColor('temperate');
-    } else if (newTemperature > 60 && newTemperature <= 75) {
-      setTemperatureColor('warm');
-    } else if (newTemperature > 75 && newTemperature <= 98) {
-      setTemperatureColor('hot');
-    }
-  };
-  
-  const decreaseTemperature = () => {
-    const newTemperature = temperatureValue - 1;
-    setTemperatureValue(temperatureValue - 1);
-  
-    if (newTemperature > 75 && newTemperature <= 98) {
-    setTemperatureColor('hot');
-  } else if (newTemperature > 60 && newTemperature <= 75) {
-      setTemperatureColor('warm');
-  } else if (newTemperature > 50 && newTemperature <= 60) {
-      setTemperatureColor('temperate');
-  } else if (newTemperature > 40 && newTemperature <= 50) {
-    setTemperatureColor('chilly');
-  } else if (newTemperature < 30 && newTemperature >= 20) {
-      setTemperatureColor('cold');
-  };
+  return ( 
+    <div>
+      <header className='temp-header'>
+        <h1 className='temp-style'> Temperature </h1>
+      </header>
+      <main>
+        < Temperature
+        />
+      </main>
+    </div>
+  )
 };
-
-
-	return (
-    <section>
-		<div className='app-container'>
-			<div className='temperature-display-container'>
-      <div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°F</div>
-			</div>
-			<div className='button-container'>
-        <button onClick={() => setTemperatureValue(increaseTemperature)}>+</button>
-				<button onClick={() => setTemperatureValue(decreaseTemperature)}>-</button>
-			</div>
-		</div>
-    </section>
-	);
-};
-
 
 export default App;
---
+++ src/components/Temperature.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Temperature.css';
+
+
+
+const Temperature = () => {
+
+const [temperatureValue, setTemperatureValue] = useState(30);
+
+const [temperatureColor, setTemperatureColor] = useState('cold');
+
+const increaseTemperature = () => {
+  const newTemperature = temperatureValue + 1;
+  setTemperatureValue(newTemperature);
+
+  // UPDATING THE COLORS TO DIFFERENT TEMPERATURES
+  if (newTemperature > 40 && newTemperature <= 50) {
+    setTemperatureColor('chilly');
+  } else if (newTemperature > 50 && newTemperature <= 60) {
+    setTemperatureColor('temperate');
+  } else if (newTemperature > 60 && newTemperature <= 75) {
+    setTemperatureColor('warm');
+  } else if (newTemperature > 75 && newTemperature <= 98) {
+    setTemperatureColor('hot');
+  }
+};
+
+const decreaseTemperature = () => {
+  const newTemperature = temperatureValue - 1;
+  setTemperatureValue(temperatureValue - 1);
+
+  if (newTemperature > 75 && newTemperature <= 98) {
+  setTemperatureColor('hot');
+} else if (newTemperature > 60 && newTemperature <= 75) {
+    setTemperatureColor('warm');
+} else if (newTemperature > 50 && newTemperature <= 60) {
+    setTemperatureColor('temperate');
+} else if (newTemperature > 40 && newTemperature <= 50) {
+  setTemperatureColor('chilly');
+} else if (newTemperature < 30 && newTemperature >= 20) {
+    setTemperatureColor('cold');
+};
+};
+
+
+return (
+<section>
+    <div className='app-container'>
+        <div className='temperature-display-container'>
+<div className={`temperature-display ${temperatureColor}`}>{temperatureValue}°F</div>
+        </div>
+        <div className='button-container'>
+    <button onClick={() => setTemperatureValue(increaseTemperature)}>+</button>
+            <button onClick={() => setTemperatureValue(decreaseTemperature)}>-</button>
+        </div>
+    </div>
+</section>
+);
+};
+
+export default Temperature;
